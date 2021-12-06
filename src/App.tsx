a83133824b8a97d681cd7645484554e9ed06e118
ewalis1 <73714272+ewalis1@users.noreply.github.com>
2021-12-06 19:11:20 +0100

    added mui library and styled components for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 import Form from './components/Form';
-import Table from './components/Table';
+import TableComponent from './components/Table';
+import {AppBar, Box, Toolbar, Typography, IconButton} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <div>
-        <Link to="/">Table</Link>
-        <Link to="/form">Form</Link>
-      </div>
+      <Box sx={{flexGrow: 1}}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{mr: 2}}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{flexGrow: 1 / 25}}>
+              <Link
+                to="/"
+                style={{
+                  color: '#fff',
+                  textDecoration: 'none',
+                  cursor: 'pointer',
+                }}
+              >
+                Table
+              </Link>
+            </Typography>
+            <Typography variant="h6" component="div" sx={{flexGrow: 1 / 25}}>
+              <Link
+                to="/form"
+                style={{
+                  color: '#fff',
+                  textDecoration: 'none',
+                  cursor: 'pointer',
+                }}
+              >
+                Form
+              </Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Routes>
-        <Route path="/" element={<Table />} />
+        <Route path="/" element={<TableComponent />} />
         <Route path="/form" element={<Form />} />
       </Routes>
     </div>
