4f41c0150ee2951373c2e385e37d562939b99806
ewalis1 <73714272+ewalis1@users.noreply.github.com>
2021-12-16 10:20:26 +0100

    form is displayed, added filter and checkboxes to the table

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,94 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {styled, Box, Paper, Grid, TextField, Button} from '@mui/material';
+
+// const Item = styled(Paper)(({theme}) => ({
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   marginTop: '20px',
+//   color: theme.palette.text.secondary,
+// }));
+
+// interface IForm {
+//   name: string;
+//   lastName: string;
+//   email: string;
+//   age: number;
+//   gender: string;
+//   phoneNumber: number;
+//   address: string;
+//   dateOfBirth: string;
+//   hobbies: [];
+// }
 
 const Form: React.FC = () => {
-  return <div className="Form">form</div>;
+  //   const [formValues, setFormValues] = useState<IForm[]>({
+  //     name: '',
+  //     lastName: '',
+  //     email: '',
+  //     age: '',
+  //     gender: '',
+  //     phoneNumber: '',
+  //     address: '',
+  //     dateOfBirth: '',
+  //     hobbies: [],
+  //   });
+
+  return (
+    <div className="Form">
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': {m: 1, width: '40ch'},
+        }}
+        autoComplete="on"
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={8}>
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              required
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField id="outlined-basic" label="Surname" variant="outlined" />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField id="outlined-basic" label="Email" variant="outlined" />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField id="outlined-basic" label="Age" variant="outlined" />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField id="outlined-basic" label="Gender" variant="outlined" />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              id="outlined-basic"
+              label="Phone number"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField id="outlined-basic" label="Address" variant="outlined" />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              id="outlined-basic"
+              label="Date of Birth"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField id="outlined-basic" label="Hobbies" variant="outlined" />
+          </Grid>
+        </Grid>
+        <Button variant="outlined">Send</Button>
+      </Box>
+    </div>
+  );
 };
 
 export default Form;
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useEffect, useState} from 'react';
 import {
+  TextField,
   Table,
   TableBody,
   TableCell,
@@ -8,8 +9,8 @@ import {
   TableHead,
   TableRow,
   Paper,
-  FormGroup,
-  FormControlLabel,
+  //   FormGroup,
+  // FormControlLabel,
   Checkbox,
 } from '@mui/material';
 
@@ -30,9 +31,20 @@ interface IHobby {
   id: string;
 }
 
+interface ISearch {
+  name: string;
+  email: string;
+  age: number;
+  address: string;
+  dateOfBirth: string;
+  hobbies: [];
+}
+
 const TableComponent: React.FC = () => {
   const [users, setUsers] = useState<IUser[]>([]);
   const [hobby, setHobby] = useState<IHobby[]>([]);
+  const [search, setSearch] = useState<ISearch[]>([]);
+  const [checked, setChecked] = useState([true, false]);
 
   useEffect(() => {
     fetch('http://localhost:3001/users')
@@ -62,49 +74,79 @@ const TableComponent: React.FC = () => {
       });
   }, []);
 
+  const label = {inputProps: {'aria-label': 'Checkbox demo'}};
+
   return (
     <div className="table">
+      <TextField
+        id="outlined-basic"
+        label="Search here"
+        variant="outlined"
+        onChange={(e) => setSearch(e.target.value as any)}
+        style={{margin: '20px 0'}}
+      />
       <TableContainer component={Paper}>
         <Table sx={{minWidth: 650}} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="left">Imię i nazwisko</TableCell>
+              <TableCell align="left">Name and Surname</TableCell>
               <TableCell align="left">Email</TableCell>
-              <TableCell align="left">Wiek</TableCell>
-              <TableCell align="left">Płeć</TableCell>
-              <TableCell align="left">Numer telefonu</TableCell>
-              <TableCell align="left">Adres</TableCell>
-              <TableCell align="left">Data urodzenia</TableCell>
-              <TableCell align="left">Zainteresowania</TableCell>
-              <TableCell align="left">Akcja</TableCell>
+              <TableCell align="left">Age</TableCell>
+              <TableCell align="left">Gender</TableCell>
+              <TableCell align="left">Phone number</TableCell>
+              <TableCell align="left">Address</TableCell>
+              <TableCell align="left">Date of birth</TableCell>
+              <TableCell align="left">Hobbies</TableCell>
+              <TableCell align="left">Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {users.map((user) => (
-              <TableRow
-                key={user.name}
-                sx={{'&:last-child td, &:last-child th': {border: 0}}}
-              >
-                <TableCell component="th" scope="row">
-                  {`${user.name} ${user.lastName}`}
-                </TableCell>
-                <TableCell align="left">{user.email}</TableCell>
-                <TableCell align="left">{user.age}</TableCell>
-                <TableCell align="left">{user.gender}</TableCell>
-                <TableCell align="left">{user.phoneNumber}</TableCell>
-                <TableCell align="left">{user.address}</TableCell>
-                <TableCell align="left">{user.dateOfBirth}</TableCell>
-                <TableCell align="left">
-                  {hobby.map((hob) => {
-                    if (user.hobbies.includes(hob.id as never)) {
-                      return hob.name + '; ';
-                    } else {
-                      return '';
-                    }
-                  })}
-                </TableCell>
-              </TableRow>
-            ))}
+            {users
+              .filter((user) => {
+                if (search === ('' as any)) {
+                  return user;
+                } else if (user.name.toLowerCase().includes(search as any)) {
+                  return user;
+                } else if (user.email.toLowerCase().includes(search as any)) {
+                  return user;
+                } else if (user.address.toLowerCase().includes(search as any)) {
+                  return user;
+                } else if (
+                  user.dateOfBirth.toLowerCase().includes(search as any)
+                ) {
+                  return user;
+                } else {
+                  return '';
+                }
+              })
+              .map((user) => (
+                <TableRow
+                  key={user.name}
+                  sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                >
+                  <TableCell component="th" scope="row">
+                    {`${user.name} ${user.lastName}`}
+                  </TableCell>
+                  <TableCell align="left">{user.email}</TableCell>
+                  <TableCell align="left">{user.age}</TableCell>
+                  <TableCell align="left">{user.gender}</TableCell>
+                  <TableCell align="left">{user.phoneNumber}</TableCell>
+                  <TableCell align="left">{user.address}</TableCell>
+                  <TableCell align="left">{user.dateOfBirth}</TableCell>
+                  <TableCell align="left">
+                    {hobby.map((hob) => {
+                      if (user.hobbies.includes(hob.id as never)) {
+                        return hob.name + '; ';
+                      } else {
+                        return '';
+                      }
+                    })}
+                  </TableCell>
+                  <TableCell align="left">
+                    <Checkbox {...label} />
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
